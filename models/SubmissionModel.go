dc188199680fba6c51aa280a84fc41208db7174d
Tanush Golwala <tanushgolwala@gmail.com>
2025-09-01 17:04:06 +0530

    fix : idx gorm skissue

--- models/SubmissionModel.go
+++ models/SubmissionModel.go
@@ -6,7 +6,7 @@ type Submission struct {
 	BaseModel
 	PPTURL      string     `gorm:"type:varchar(255)" json:"ppt_url" valid:"required~PPT URL is required"`
 	Description *string    `gorm:"type:text" json:"description,omitempty"`
-	RoundID     uuid.UUID  `gorm:"type:uuid;constraint:OnDelete:CASCADE,OnUpdate:CASCADE;unique" json:"round_id"`
+	RoundID     uuid.UUID  `gorm:"type:uuid;constraint:OnDelete:CASCADE,OnUpdate:CASCADE;uniqueIndex:idx_team_round" json:"round_id"`
 	TeamID      *uuid.UUID `gorm:"uniqueIndex:idx_team_round" json:"team_id"`
 	Team        Team       `gorm:"foreignKey:TeamID" json:"team"`
 }
